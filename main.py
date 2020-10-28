aadba2f9ccfecd0fd85db896462191e04f53abcc
Alan <sinderpl@gmail.com>
2020-10-28 20:35:31 +0000

    Tuple filenames implemented

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ Created on Wed Oct 28 20:16:31 2020
 @author: alann
 """
 
+filenames = ("dataSet/securities.csv",
+             "dataSet/fundamentals.csv",
+             "dataSet/prices.csv",
+             "dataSet/prices-split-adjusted.csv")
 try:
-    with open("dataSet/securitiess.csv", encoding='cp1252') as securities:
-        for security in securities:
-            print(security)
+    with open(filenames[0], encoding='cp1252') as securities:
+        print("file loaded")
+        # for security in securities:
+            # print(security)
 except FileNotFoundError:
     print("File could not be found")
