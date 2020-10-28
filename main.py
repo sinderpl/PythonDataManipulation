128524b8eda40b8e3fa759fb5c580eb80e3e1f64
Alan <sinderpl@gmail.com>
2020-10-28 20:42:03 +0000

    column generation file

--- main.py
+++ main.py
@@ -9,9 +9,19 @@ filenames = ("dataSet/securities.csv",
              "dataSet/fundamentals.csv",
              "dataSet/prices.csv",
              "dataSet/prices-split-adjusted.csv")
+
 try:
-    with open(filenames[0], encoding='cp1252') as securities:
+    with open(filenames[1], encoding='cp1252') as securities:
         print("file loaded")
+        
+        
+        #Get column names
+        # print(securities.readline().split(","))
+        column_names = (securities.readline())
+        for column in column_names.split(","):
+            print(column)
+        
+        #Print all
         # for security in securities:
             # print(security)
 except FileNotFoundError:
